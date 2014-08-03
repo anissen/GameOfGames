e6a623061175e8e4b94141f6619dfff0ece57582
Anders Nissen <andnis@gmail.com>
2014-08-03 20:00:50 +0200

    Hex chain improvements

--- source/com/andersnissen/games/HexChain.hx
+++ source/com/andersnissen/games/HexChain.hx
@@ -16,7 +16,7 @@ class HexChain extends GameState
 {
     var hexSprites :FlxSpriteGroup;
     var hexToCollect :Array<FlxSprite>;
-    var hexRadius :Int = 41;
+    var hexRadius :Int = 52;
 
     var chainLength :Int = 5;
     var chain :Array<FlxSprite>;
@@ -33,16 +33,34 @@ class HexChain extends GameState
 
         chain = new Array<FlxSprite>();
 
-        var collectableDotMap :Array<Array<Bool>> = [ for (x in 0...5) [ for (y in 0...9) false ]];
-        var collectableDotCount = 0;
-        while (collectableDotCount < 20) {
-            var x = FlxRandom.intRanged(0, 4);
-            var y = FlxRandom.intRanged(0, 6);
-            if (collectableDotMap[x][y] == false) {
-                collectableDotMap[x][y] = true;
-                collectableDotCount++;
+        var collectableHexMap :Array<Array<Bool>> = [ for (x in 0...4) [ for (y in 0...7) false ]];
+        var collectableHexCount = 0;
+        
+        function makeChain(x :Int, y :Int) {
+            if (collectableHexCount > 8) return;
+
+            if (x < 0 || x > 3) return;
+            if (y < 0 || y > 6) return;
+            if (x == 3 && y % 2 == 0) return;
+            if (collectableHexMap[x][y] == true) return;
+
+            collectableHexMap[x][y] = true;
+            collectableHexCount++;
+            if (collectableHexCount > 8) return;
+
+            var neighborsX = FlxRandom.shuffleArray([x - 1, x, x + 1], 5);
+            var neighborsY = FlxRandom.shuffleArray([y - 1, y, y + 1], 5);
+            for (nX in neighborsX) {
+                for (nY in neighborsY) {
+                    if (collectableHexCount <= 8)
+                        makeChain(nX, nY);
+                }
             }
         }
+
+        var x = FlxRandom.intRanged(1, 2);
+        var y = FlxRandom.intRanged(2, 4);
+        makeChain(x, y);
 
         var margin = 2;
         var hexHeight = hexRadius * 2;
@@ -58,25 +76,25 @@ class HexChain extends GameState
             polys.push(new FlxPoint(posX, posY));
         }
 
-        for (y in 0...9) {
-            for (x in 0...5) {
-                if (x == 4 && y % 2 == 1) continue;
+        for (y in 0...7) {
+            for (x in 0...4) {
+                if (x == 3 && y % 2 == 0) continue;
                 
-                var hex = new FlxSprite((y % 2 == 0 ? 0 : horizontalDistance / 2) + x * horizontalDistance, y * verticalDistance);
+                var hex = new FlxSprite((y % 2 == 0 ? horizontalDistance / 2 : 0) + x * horizontalDistance, y * verticalDistance);
                 hex.makeGraphic(Math.ceil(hexWidth), Math.ceil(hexHeight), FlxColor.TRANSPARENT, true);
 
-                var isCollectableDot = collectableDotMap[x][y];
+                var isCollectableHex = collectableHexMap[x][y];
                 var color;
-                if (isCollectableDot) {
+                if (isCollectableHex) {
                     color = FlxColor.BLUE;
                     hexToCollect.push(hex);
                 } else {
-                    color = FlxRandom.color();
+                    color = FlxRandom.getObject([FlxColor.RED, FlxColor.GREEN, FlxColor.YELLOW, FlxColor.CYAN]);
                 }
 
-                hex.drawPolygon(polys.copy(), color); //, { color: FlxColor.BLACK, thickness: 0.0 } );
+                hex.drawPolygon(polys.copy(), color);
 
-                // flixel.tweens.FlxTween.tween(hex.scale, { x: -1 }, 2, { startDelay: (y + x) / 10 });
+                // flixel.tweens.FlxTween.tween(hex.scale, { x: -1 }, 1, { startDelay: (y + x) / 10 });
                 hexSprites.add(hex);
             }
         }
@@ -88,8 +106,10 @@ class HexChain extends GameState
         add(chainSprite);
     }
 
-    override public function update():Void
+    override public function update() :Void
     {
+        if (!gameActive) return;
+
         super.update();
 
         #if !FLX_NO_TOUCH
@@ -102,13 +122,6 @@ class HexChain extends GameState
                     hexTouched(hex, touch.getWorldPosition());
                 });
             }
-            if (touch.justReleased) {
-                if (chain.length == chainLength) {
-                    win();
-                } else {
-                    lose();
-                }
-            }
         }
         #else
         if (FlxG.mouse.pressed) {
@@ -116,12 +129,6 @@ class HexChain extends GameState
                 if (!hex.overlapsPoint(FlxG.mouse.getWorldPosition())) return;
                 hexTouched(hex, FlxG.mouse.getWorldPosition());
             });
-        } else if (FlxG.mouse.justReleased) {
-            if (chain.length == chainLength) {
-                win();
-            } else {
-                lose();
-            }
         }
         #end
     }
@@ -154,19 +161,25 @@ class HexChain extends GameState
             chain.push(hex);
             FlxTween.tween(hex, { alpha: 0 }, 0.4);
             FlxTween.tween(hex.scale, { x: 0, y: 0 }, 0.4);
-        }
 
-        if (chain.length == chainLength) {
-            success(chain[chain.length - 1].getMidpoint());
+            success(hex.getMidpoint());
+
+            if (chain.length == hexToCollect.length) {
+                win();
+            }
         }
 
         chainSprite.fill(FlxColor.TRANSPARENT);
         // Draw a dot if there is only one hex in the chain
         if (chain.length == 1) {
-            chainSprite.drawCircle(chain[0].getMidpoint().x, chain[0].getMidpoint().y, hexRadius / 2, FlxColor.YELLOW);
+            chainSprite.drawCircle(chain[0].getMidpoint().x, chain[0].getMidpoint().y, hexRadius / 2, FlxColor.BLACK);
+            chainSprite.drawCircle(chain[0].getMidpoint().x, chain[0].getMidpoint().y, hexRadius / 2 - 5, FlxColor.YELLOW);
         }
         for (chainIndex in 1...chain.length) {
-            chainSprite.drawLine(chain[chainIndex - 1].getMidpoint().x, chain[chainIndex - 1].getMidpoint().y, chain[chainIndex].getMidpoint().x, chain[chainIndex].getMidpoint().y, { color: FlxColor.YELLOW, thickness: hexRadius } );
+            chainSprite.drawLine(chain[chainIndex - 1].getMidpoint().x, chain[chainIndex - 1].getMidpoint().y, chain[chainIndex].getMidpoint().x, chain[chainIndex].getMidpoint().y, { color: FlxColor.BLACK, thickness: hexRadius } );
+        }
+        for (chainIndex in 1...chain.length) {
+            chainSprite.drawLine(chain[chainIndex - 1].getMidpoint().x, chain[chainIndex - 1].getMidpoint().y, chain[chainIndex].getMidpoint().x, chain[chainIndex].getMidpoint().y, { color: FlxColor.YELLOW, thickness: hexRadius - 5 } );
         }
     }
 }
