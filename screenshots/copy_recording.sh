cb49db13f84a6619dba2f1f2cc203863b92ccde6
Anders Nissen <andnis@gmail.com>
2014-11-14 21:33:01 +0100

    Misc

--- screenshots/copy_recording.sh
+++
@@ -1,5 +0,0 @@
-echo -e "Filename for recording: \c "
-read filename
-echo "Saving to ${filename}.mp4"
-adb pull /sdcard/${filename}.mp4 ${filename}.mp4
-echo "Done"
--- screenshots/record_android.sh
+++
@@ -1,4 +0,0 @@
-echo -e "Filename for recording: \c "
-read filename
-echo "Recording screen..."
-adb shell screenrecord --bit-rate 6000000 /sdcard/${filename}.mp4
